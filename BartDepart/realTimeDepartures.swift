f740df5f7ac31b09db4176ffeb3281a6354ac9b7
Pac12Run8000 <norbert298@gmail.com>
2016-04-06 21:22:02 -0700

    version 0.04

--- BartDepart/realTimeDepartures.swift
+++ BartDepart/realTimeDepartures.swift
@@ -8,8 +8,86 @@
 
 import Foundation
 
-class realTimeDepartures : UIViewController {
+class realTimeDepartures : UIViewController, NSXMLParserDelegate {
+    @IBOutlet weak var tbData: UITableView!
+    
+    
+    var parser = NSXMLParser()
+    var posts = NSMutableArray()
+    var elements = NSMutableDictionary()
+    var element = NSString()
+    var title1 = NSMutableString()
+    var date = NSMutableString()
+    
+    
     override func viewDidLoad() {
+        
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        
+        
+        self.beginParsing()
     }
-}
+    /*************************/
+    func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]) {
+        element = elementName
+        if (elementName as NSString).isEqualToString("item")
+        {
+            elements = NSMutableDictionary()
+            elements = [:]
+            title1 = NSMutableString()
+            title1 = ""
+            date = NSMutableString()
+            date = ""
+        }
+    }
+   
+    func parser(parser: NSXMLParser, foundCharacters string: String)
+    {
+        if element.isEqualToString("title") {
+            title1.appendString(string)
+        } else if element.isEqualToString("pubDate") {
+            date.appendString(string)
+        }
+    }
+    
+    func parser(parser: NSXMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        if (elementName as NSString).isEqualToString("item") {
+            if !title1.isEqual(nil) {
+                elements.setObject(title1, forKey: "title")
+            }
+            if !date.isEqual(nil) {
+                elements.setObject(date, forKey: "date")
+            }
+            posts.addObject(elements)
+        }
+    }
+    /*************************/
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return posts.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        var cell : UITableViewCell = tableView.dequeueReusableCellWithIdentifier("Cell")!
+        if(cell.isEqual(NSNull)) {
+            cell = NSBundle.mainBundle().loadNibNamed("Cell", owner: self, options: nil)[0] as! UITableViewCell;
+        }
+        
+        cell.textLabel?.text = posts.objectAtIndex(indexPath.row).valueForKey("title") as? String
+        cell.detailTextLabel?.text = posts.objectAtIndex(indexPath.row).valueForKey("date") as? String
+        
+        return cell as UITableViewCell
+    }
+    
+    func beginParsing()
+    {
+        posts = []
+        parser = NSXMLParser(contentsOfURL:(NSURL(string:"http://images.apple.com/main/rss/hotnews/hotnews.rss"))!)!
+        parser.delegate = self
+        parser.parse()
+        tbData!.reloadData()
+        }
+    }
